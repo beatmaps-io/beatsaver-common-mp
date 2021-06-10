93e79b031fe12b8c6c0d547da3ee70be526d6bee
Thomas Cheyney <top_cat@thomasc.co.uk>
2021-06-10 17:13:55 +0100

    Kotlin 1.5.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.4.31"
-    kotlin("plugin.serialization") version "1.4.31"
+    kotlin("multiplatform") version "1.5.10"
+    kotlin("plugin.serialization") version "1.5.10"
 }
 
 group = "io.beatmaps"
