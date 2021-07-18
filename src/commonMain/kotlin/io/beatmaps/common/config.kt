cebe4e9b23bb507a6bd3c8a88c5d814503eca8e0
Thomas Cheyney <top_cat@thomasc.co.uk>
2021-07-18 13:46:15 +0100

    Add remote api config

--- src/commonMain/kotlin/io/beatmaps/common/config.kt
+++ src/commonMain/kotlin/io/beatmaps/common/config.kt
@@ -3,6 +3,7 @@ package io.beatmaps.common
 object Config {
     const val basename: String = "https://beatmaps.io"
     const val cdnbase: String = "https://cdn.beatmaps.io"
-    const val apibase: String = "/api"//https://api.beatmaps.io"
+    const val apibase: String = "/api"
+    const val apiremotebase: String = "https://api.beatmaps.io"
     const val oneClick: Boolean = true
 }
