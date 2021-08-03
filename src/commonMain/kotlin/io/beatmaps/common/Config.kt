b6de50309d4a498815afb5324fed5036651483b6
Thomas Cheyney <top_cat@thomasc.co.uk>
2021-08-03 20:57:02 +0100

    Update some base config

--- src/commonMain/kotlin/io/beatmaps/common/Config.kt
+++ src/commonMain/kotlin/io/beatmaps/common/Config.kt
@@ -2,8 +2,8 @@ package io.beatmaps.common
 
 object Config {
     const val basename: String = "https://beatmaps.io"
-    const val cdnbase: String = "https://cdn.beatmaps.io"
+    const val cdnbase: String = "https://cdn.beatsaver.com"
     const val apibase: String = "/api"
-    const val apiremotebase: String = "https://api.beatmaps.io"
+    const val apiremotebase: String = "https://api.beatsaver.com"
     const val oneClick: Boolean = true
 }
