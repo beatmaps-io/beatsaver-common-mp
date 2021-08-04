94264a07b6108a102d26d3f20030ec0bcb339284
Thomas Cheyney <top_cat@thomasc.co.uk>
2021-08-04 20:53:49 +0100

    Update basename

--- src/commonMain/kotlin/io/beatmaps/common/Config.kt
+++ src/commonMain/kotlin/io/beatmaps/common/Config.kt
@@ -1,7 +1,7 @@
 package io.beatmaps.common
 
 object Config {
-    const val basename: String = "https://beatmaps.io"
+    const val basename: String = "https://beatsaver.com"
     const val cdnbase: String = "https://cdn.beatsaver.com"
     const val apibase: String = "/api"
     const val apiremotebase: String = "https://api.beatsaver.com"
