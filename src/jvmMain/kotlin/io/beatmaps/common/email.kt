03a4b28eefe0b481630401e92106152a7fb213b9
Thomas Cheyney <top_cat@thomasc.co.uk>
2022-08-29 13:36:47 +0100

    Move email sending to queue

--- src/jvmMain/kotlin/io/beatmaps/common/email.kt
+++ src/jvmMain/kotlin/io/beatmaps/common/email.kt
@@ -1,5 +1,9 @@
 package io.beatmaps.common
 
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.util.pipeline.PipelineContext
 import org.apache.commons.mail.DefaultAuthenticator
 import org.apache.commons.mail.SimpleEmail
 import java.util.logging.Logger
@@ -9,22 +13,34 @@ val relayUsername: String? = System.getenv("RELAY_USERNAME")
 val relayPassword: String? = System.getenv("RELAY_PASSWORD")
 private val emailLogger = Logger.getLogger("bmio.Email")
 
-fun sendEmail(to: String, subject: String, body: String) {
+data class EmailInfo(val to: String, val subject: String, val body: String)
+
+fun genericEmail() = SimpleEmail().apply {
+    hostName = relayHostname
+    setSmtpPort(465)
+    setAuthenticator(DefaultAuthenticator(relayUsername, relayPassword))
+    isSSLOnConnect = true
+    setFrom("no-reply@beatsaver.com")
+}
+
+fun Application.emailQueue() {
+    rabbitOptional {
+        consumeAck("email", EmailInfo::class) { _, emailInfo ->
+            genericEmail().apply {
+                subject = emailInfo.subject
+                setMsg(emailInfo.body)
+                addTo(emailInfo.to)
+            }.send()
+        }
+    }
+}
+
+fun PipelineContext<Unit, ApplicationCall>.sendEmail(to: String, subject: String, body: String) {
     if (relayHostname == null) {
         emailLogger.warning("Email not setup")
         emailLogger.info(body)
         return
     }
 
-    val email = SimpleEmail()
-
-    email.hostName = relayHostname
-    email.setSmtpPort(465)
-    email.setAuthenticator(DefaultAuthenticator(relayUsername, relayPassword))
-    email.isSSLOnConnect = true
-    email.setFrom("no-reply@beatsaver.com")
-    email.subject = subject
-    email.setMsg(body)
-    email.addTo(to)
-    email.send()
+    call.pub("beatmaps", "email", null, EmailInfo(to, subject, body))
 }
