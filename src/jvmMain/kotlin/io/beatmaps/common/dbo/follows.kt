34db7aa88658c4fd7759f4378030a7f57978ad11
Thomas Cheyney <top_cat@thomasc.co.uk>
2022-09-07 16:18:52 +0100

    Add FollowsDao

--- src/jvmMain/kotlin/io/beatmaps/common/dbo/follows.kt
+++ src/jvmMain/kotlin/io/beatmaps/common/dbo/follows.kt
@@ -1,11 +1,22 @@
 package io.beatmaps.common.dbo
 
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.javatime.timestamp
 
-object Follows: IntIdTable("follows", "followId") {
+object Follows : IntIdTable("follows", "followId") {
     val userId = reference("userId", User)
     val followerId = reference("followerId", User)
 
     val since = timestamp("since")
+}
+
+data class FollowsDao(val key: EntityID<Int>) : IntEntity(key) {
+    companion object : IntEntityClass<FollowsDao>(Follows)
+    val user by UserDao referencedOn Follows.userId
+    val follower by UserDao referencedOn Follows.followerId
+
+    val since by Follows.since
 }
