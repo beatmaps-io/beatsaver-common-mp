dcff98f2dfc324aac1f51e9b53c55154e3f3cfc0
Pixelguymm <maartenmarx@hotmail.be>
2023-06-27 17:08:39 +0200

    Add Collaborations API

--- src/jvmMain/kotlin/io/beatmaps/common/dbo/collaboration.kt
+++ src/jvmMain/kotlin/io/beatmaps/common/dbo/collaboration.kt
@@ -1,5 +1,8 @@
 package io.beatmaps.common.dbo
 
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Index
 
@@ -9,4 +12,11 @@ object Collaboration : IntIdTable("collaboration", "collaborationId") {
     val accepted = bool("accepted")
 
     val link = Index(listOf(mapId, collaboratorId), true, "collaborationLink")
+}
+
+data class CollaborationDAO(val key: EntityID<Int>) : IntEntity(key) {
+    companion object : IntEntityClass<CollaborationDAO>(Collaboration)
+    val mapId by Collaboration.mapId
+    val collaboratorId by Collaboration.collaboratorId
+    val accepted by Collaboration.accepted
 }
