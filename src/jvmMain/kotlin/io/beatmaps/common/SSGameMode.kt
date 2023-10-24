f75f7a8871d7b9eb27c3ac838f2bbc9c3196a3ea
Thomas Cheyney <top_cat@thomasc.co.uk>
2023-10-24 08:46:51 +0100

    Fix legacy leaderboards. Fixes beatmaps-io/beatsaver-main#224

--- src/jvmMain/kotlin/io/beatmaps/common/SSGameMode.kt
+++ src/jvmMain/kotlin/io/beatmaps/common/SSGameMode.kt
@@ -5,7 +5,7 @@ import io.beatmaps.common.api.ECharacteristic
 enum class SSGameMode(val idx: Int, val characteristic: ECharacteristic) {
     SoloStandard(0, ECharacteristic.Standard), SoloOneSaber(1, ECharacteristic.OneSaber), SoloNoArrows(2, ECharacteristic.NoArrows),
     Solo90Degree(3, ECharacteristic._90Degree), Solo360Degree(4, ECharacteristic._360Degree), SoloLightshow(5, ECharacteristic.Lightshow),
-    SoloLawless(6, ECharacteristic.Lawless);
+    SoloLawless(6, ECharacteristic.Lawless), SoloLegacy(7, ECharacteristic.Legacy);
 
     companion object {
         private val map = values().associateBy(SSGameMode::idx)
