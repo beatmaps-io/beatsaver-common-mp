4b0b31e2860cab29a0a43c143350644f71d00ed1
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-07-24 20:28:40 +0100

    Add option to ellipsize to not treat some characters as half width

--- src/commonMain/kotlin/io/beatmaps/common/util/text.kt
+++ src/commonMain/kotlin/io/beatmaps/common/util/text.kt
@@ -5,22 +5,24 @@ import kotlin.math.max
 object TextHelper {
     private const val NON_THIN: String = "[^iIl1\\.,']"
 
-    private fun textWidth(str: String): Int {
-        return (str.length - str.replace(NON_THIN.toRegex(), "").length / 2)
+    private fun textWidth(str: String, monoWidth: Boolean) = if (monoWidth) {
+        str.length
+    } else {
+        str.length - str.replace(NON_THIN.toRegex(), "").length / 2
     }
 
-    private tailrec fun stepForward(text: String, end: Int, max: Int): Int {
+    private tailrec fun stepForward(text: String, end: Int, max: Int, monoWidth: Boolean): Int {
         val newEnd = text.indexOf(' ', end + 1).takeIf { it >= 0 } ?: text.length
 
-        return if (textWidth(text.substring(0, newEnd) + "...") < max) {
-            stepForward(text, newEnd, max)
+        return if (textWidth(text.substring(0, newEnd) + "...", monoWidth) < max) {
+            stepForward(text, newEnd, max, monoWidth)
         } else {
             end
         }
     }
 
-    fun ellipsize(text: String, max: Int) =
-        if (textWidth(text) <= max) {
+    fun ellipsize(text: String, max: Int, monoWidth: Boolean = false) =
+        if (textWidth(text, monoWidth) <= max) {
             text
         } else {
             val end = text.lastIndexOf(' ', max - 3)
@@ -29,7 +31,7 @@ object TextHelper {
                 // Just one long word. Chop it off.
                 max(0, max - 3)
             } else {
-                stepForward(text, end, max)
+                stepForward(text, end, max, monoWidth)
             }
 
             "${text.take(newEnd)}..."
