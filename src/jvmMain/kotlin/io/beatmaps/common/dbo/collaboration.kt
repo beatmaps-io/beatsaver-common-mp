ce8479dfbd8ba8798e0e63095d75063f06fccf9b
Thomas Cheyney <top_cat@thomasc.co.uk>
2025-07-31 13:53:16 +0100

    Add method to delete unaccepted collaboration requests on a map

--- src/jvmMain/kotlin/io/beatmaps/common/dbo/collaboration.kt
+++ src/jvmMain/kotlin/io/beatmaps/common/dbo/collaboration.kt
@@ -1,11 +1,15 @@
 package io.beatmaps.common.dbo
 
+import io.beatmaps.common.db.deleteReturningWhere
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Index
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.javatime.timestamp
+import org.jetbrains.exposed.sql.not
 
 object Collaboration : IntIdTable("collaboration", "collaborationId") {
     val mapId = reference("mapId", Beatmap)
@@ -14,6 +18,11 @@ object Collaboration : IntIdTable("collaboration", "collaborationId") {
     val accepted = bool("accepted")
 
     val link = Index(listOf(mapId, collaboratorId), true, "collaborationLink")
+
+    fun deleteForMap(id: Int) =
+        Collaboration.deleteReturningWhere({ mapId eq id and not(accepted) }, collaboratorId).map {
+            it[collaboratorId].value
+        }
 }
 
 data class CollaborationDao(val key: EntityID<Int>) : IntEntity(key) {
